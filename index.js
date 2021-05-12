2dc3b3a61e9a096371bad5b6f306756252ec4ac3
CeyhunGulbas <cetree61@gmail.com>
2021-05-12 18:03:44 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(){
+    return 2000/100
+}
+console.log(divide())
+
+
+let square = (para1) => para1 ** 2
+console.log(square(5))
+
+let add = (param1,param2) => param1 + param2
+console.log(add(5,12))
