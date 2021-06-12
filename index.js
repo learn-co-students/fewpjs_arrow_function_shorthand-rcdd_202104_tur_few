195c5a28bff23f6ec3a6b7d8054ed07aec081a24
abdulrahman <abdulrahmantaji99@gmail.com>
2021-06-12 23:46:49 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+// function divide(){
+//     return 2000/100
+// }
+
+// let add = (numOne, numTwo)=> {
+//     return numOne + numTwo
+// }
+// let square = (number)=> {
+//     return number * number
+// }
+
+let divide = () => 2000 / 100
+let square = x => x * x;
+let add = (a, b) => a + b;
